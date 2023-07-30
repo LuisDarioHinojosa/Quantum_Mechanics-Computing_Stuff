43d02371cab579bf26b38ae10e3f006a80115d46
Q-b1t <darioluishinojosaq@mail.com>
2023-07-30 01:31:09 -0600

    superdence coding function implementation

--- modules/utils/algorithms_utils.py
+++ modules/utils/algorithms_utils.py
@@ -6,6 +6,8 @@ import qiskit_textbook.tools as qt
 import os
 import sys
 
+
+# added to be able to use the experiment_utils script.  
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 
@@ -18,56 +20,94 @@ This functions where retrieved from qiskit documentation
 """
 
 def qft_rotations(circuit, n):
-    """Performs qft on the first n qubits in circuit (without swaps)"""
-    if n == 0:
-        return circuit
-    n -= 1
-    circuit.h(n)
-    for qubit in range(n):
-        circuit.cp(pi/2**(n-qubit), qubit, n)
-    # At the end of our function, we call the same function again on
-    # the next qubits (we reduced n by one earlier in the function)
-    qft_rotations(circuit, n)
-def swap_registers(circuit, n):
-    for qubit in range(n//2):
-        circuit.swap(qubit, n-qubit-1)
+  """Performs qft on the first n qubits in circuit (without swaps)"""
+  if n == 0:
     return circuit
+  n -= 1
+  circuit.h(n)
+  for qubit in range(n):
+    circuit.cp(pi/2**(n-qubit), qubit, n)
+  # At the end of our function, we call the same function again on
+  # the next qubits (we reduced n by one earlier in the function)
+  qft_rotations(circuit, n)
+
+
+def swap_registers(circuit, n):
+  for qubit in range(n//2):
+    circuit.swap(qubit, n-qubit-1)
+  return circuit
 
 def qft(circuit, n):
-    """QFT on the first n qubits in circuit"""
-    qft_rotations(circuit, n)
-    swap_registers(circuit, n)
-    return circuit
+  """QFT on the first n qubits in circuit"""
+  qft_rotations(circuit, n)
+  swap_registers(circuit, n)
+  return circuit
 
 """
 This functions are of my own authoring.
 """
+
+def superdence_coding(s):
+  """
+  Superdense coding is a quantum communication algorithm in which to classical bits are sent using a single qubit.
+  The qubit is suposed to be shared between an emisor and a receiber. It consist on one system per party, and 
+  shares an entangled state.
+  Inputs: String represented the two bit classical sequence.
+  Output: The superdence coding quantum circuit corresponding to the input sequence.
+  """
+  # initialize the quantum circuit
+  qc = QuantumCircuit(2,'00')
+  
+  # get the entangled state 
+  qc.h(0)
+  qc.cx(0,1)
+  qc.barrier()
+  
+  # get the bell state corresponding to the input sequence
+  if(s[0] == '1'):
+    qc.x(0)
+  if (s[1] == '1'):
+    qc.z(0)
+  qc.barrier()
+  
+  # remove the entangled state
+  qc.cx(0,1)
+  qc.h(0)
+  qc.barrier()
+
+  # measure the qubit
+  qc.measure(range(0,2),range(0,2))
+
+  return qc
+  
+ 
+
 def simon_circuit(s):
-    """
-    Inputs: Secret sequence s
-    Problem statement:
-    F(x) = F(y) if y = x + s
-    """
-    # get the length of the sequence
-    n = len(s)
-    # instance two qubits for each bit in the sequence
-    qc = QuantumCircuit(2*n, 2*n)
-    # apply superpositiion
-    qc.h(range(n))
-    qc.barrier()
-    # apply the unknown oracle function
-    qc.append(qt.simon_oracle(s),qc.qubits)
-    qc.barrier()
-    # measure
-    qc.measure(range(n, 2*n), range(n, 2*n))
-    qc.barrier()
-    # another superposition
-    qc.h(range(n))
-    # measurement
-    qc.measure(range(n), range(n))
-    return qc
-    
-   
+  """
+  Inputs: Secret sequence s
+  Problem statement:
+  F(x) = F(y) if y = x + s
+  """
+  # get the length of the sequence
+  n = len(s)
+  # instance two qubits for each bit in the sequence
+  qc = QuantumCircuit(2*n, 2*n)
+  # apply superpositiion
+  qc.h(range(n))
+  qc.barrier()
+  # apply the unknown oracle function
+  qc.append(qt.simon_oracle(s),qc.qubits)
+  qc.barrier()
+  # measure
+  qc.measure(range(n, 2*n), range(n, 2*n))
+  qc.barrier()
+  # another superposition
+  qc.h(range(n))
+  # measurement
+  qc.measure(range(n), range(n))
+  return qc
+  
+  
 
 
 
